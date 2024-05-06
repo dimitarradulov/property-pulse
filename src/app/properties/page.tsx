7db26117e8919db12ea1522122f77189c03f4c95
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2024-05-06 15:59:42 +0300

    add property card

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -1,4 +1,23 @@
+import properties from "@/data/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+import { Property } from "@/types/property";
+
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>;
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No Properties Found!</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
+
 export default PropertiesPage;
